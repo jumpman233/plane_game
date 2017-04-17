aab90145130ab1cc78a38c7dc0d086ada22ce742
username <463529650@qq.com>
2017-04-17 16:57:22 +0800

    approve fight annimate

--- js/app/animate/fightBk.js
+++ js/app/animate/fightBk.js
@@ -5,41 +5,104 @@
 define(['global', 'ball', 'util'], function ( global, Ball, util ) {
     'use strict';
     var width,
-        ballBaseR,
-        ballMaxR,
-        balls = [],
-        init = false;
+        height,
+        stars = [],
+        baseR = 15,
+        randomR = 15,
+        vy = 1,
+        isInit = false,
+        possibility = 0.01;
+
+    var Star = function (  ) {
+        this.x = 0;
+        this.y = 0;
+        this.vy = 0;
+        this.radius = 0;
+        this.scale = 0;
+    };
+    
+    Star.prototype = {
+        constructor: Star,
+        draw: function ( ctx ) {
+            var star = this;
+            ctx.save();
+            ctx.translate(star.x, star.y);
+            ctx.beginPath();
+            ctx.scale(star.scale,star.scale);
+            if(star.scale <= 1){
+                star.scale += 0.1;
+            }
+            ctx.arc(0, 0, star.radius, 0, Math.PI *2);
+            ctx.closePath();
+            var gradient = ctx.createRadialGradient(0, 0, 0, 0, 0, star.radius);
+            gradient.addColorStop(0, 'rgba(255,255,255,1)');
+            gradient.addColorStop(.1, 'rgba(255,255,255,1)');
+            gradient.addColorStop(.2, 'rgba(255,255,255,.5)');
+            gradient.addColorStop(.6, 'rgba(255,255,255,0)');
+            gradient.addColorStop(1, 'rgba(255,255,255,0)');
+            ctx.fillStyle = gradient;
+            ctx.fill();
+            ctx.restore();
+        },
+        move: function (  ) {
+            this.y += this.vy;
+        }
+    };
+
+    var randomStar = function ( possibility ) {
+        if(Math.random() <= possibility){
+            var star = new Star(),
+                flag = true;
+            star.radius = baseR + Math.random() * randomR;
+            star.x = Math.random() * (width - star.radius) + star.radius / 2;
+            star.y = Math.random() * (height - star.radius) + star.radius / 2;
+            star.vy = vy;
+            for(var i = 0; i < stars.length; i++){
+                if(Math.abs(stars[i].x - star.x) <= 50 ||
+                Math.abs(stars[i].y - star.y) <= 50){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                stars.push(star);
+            }
+        }
+    };
+
+    var dirtyCheck = function (  ) {
+        for(var i = 0; i < stars.length; i++){
+            if(stars[i].y - stars[i].radius / 2 >= height){
+                stars.splice(i, 1);
+            }
+        }
+    };
+
+    var init = function (  ) {
+        width = global.width;
+        height = global.height;
+        stars = [];
+        isInit = true;
+    };
 
     var draw = function ( ctx ) {
-        if(!init){
-            reset();
-            init = true;
-        }
-        if(Math.random() < 0.0005) {
-            var newBall = new Ball;
-            newBall.radius = Math.random() * ballMaxR + ballBaseR;
-            newBall.light = true;
-            newBall.color = util.resolveColor(0, 255, 255, 1);
-            newBall.x = Math.random() * (width - newBall.radius / 2) + newBall.radius / 2;
-            newBall.y = -newBall.radius;
-            newBall.vy = 0.05;
-            balls.push(newBall);
-        }
+        if(!isInit) init();
 
-        balls.forEach(function ( item ) {
-            item.draw(ctx);
-            item.move();
+        randomStar(possibility);
+        stars.forEach(function ( star ) {
+            star.draw(ctx);
+            star.move();
         });
+
+        dirtyCheck();
     };
 
     var reset = function (  ) {
-        width = global.width;
-        balls = [];
-        ballBaseR = width / 100;
-        ballMaxR = width / 150;
+        stars = [];
     };
 
     return {
+        init: init,
         reset: reset,
         draw: draw
     };
